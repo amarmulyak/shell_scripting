20778e6f28e956d45a7ad967e433ebd3cbc9a5cc
amarmulyak <a.marmuliak@gmail.com>
2020-04-09 23:44:41 +0300

    Editing s3

--- section3/practice_exercise/ex2.sh
+++ section3/practice_exercise/ex2.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-ls
+FILE=$1
 
-if [ "$?" -eq "0" ]
+if [ -f "$FILE" ]
 then
-	echo "This script will exit with a 0 exit status."
+	echo "This is a regular file"
+	exit 0
+elif [ -d "$FILE" ]
+then
+	echo "This is a directory"
+	exit 1
+else
+	echo "This is some other type of file"
+	exit 2
 fi
