3ca0f10c619b1f7608ba2ece4ed7b8d003600106
amarmulyak <a.marmuliak@gmail.com>
2020-04-15 22:25:53 +0300

    Updated s4

--- section4/practice/ex1.sh
+++ section4/practice/ex1.sh
@@ -1,7 +1,7 @@
 #1/bin/bash/
 
 function count_files () {
-	local N_FILES=$(ls -1 *.* | wc -l)
+	local N_FILES=$(ls -1 *.* | wc -l) # ls | "wc -l" is a simpler way to count files
 	echo "Folder contains $N_FILES file(s)"
 }
 
